cb87ccb551ce58a9ed56a9937a90a82c9a5755d4
Will <doggy.huang@gmail.com>
2018-07-12 11:51:19 +0800

    示範內嵌繫結、屬性繫結、事件繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  sitename = 'The Will Will Web';
+  subtitle = '記載著 Will 在網路世界的學習心得與技術分享';
+  logourl = '/assets/images/logo.png';
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.sitename = 'Hello World';
+    }, 2000);
+  }
+
+  changeTitle(evt: MouseEvent) {
+    console.log(evt);
+    if (evt.ctrlKey) {
+      this.sitename = 'The Will Will Web';
+    }
   }
 
 }
